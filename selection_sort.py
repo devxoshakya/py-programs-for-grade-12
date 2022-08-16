40f50d0a4256bd9a1f43cd0678e380e43e34d386
Dev Shakya <shakyatechnicals@gmail.com>
2022-08-16 12:54:05 +0530

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,29 +1,23 @@
 
-def selection_Sort(list2):
-        flag = 0 #to decide when to swap
-        n=len(list2)
-        for i in range(n): # Traverse through all list elements
-          min = i
-          for j in range(i + 1, len(list2)): #the left elements
-    #are already sorted in previous passes
-             if list2[j] < list2[min]: # element at j is smaller
-    #than the current min element
-                 min = j
-                 flag = 1
-        if flag == 1 : # next smallest element is found
-             list2[min], list2[i] = list2[i], list2[min]
+def selectionSort(array, size):
+    
+    for ind in range(size):
+        min_index = ind
+ 
+        for j in range(ind + 1, size):
+           
+            if array[j] < array[min_index]:
+                min_index = j
+         
+        (array[ind], array[min_index]) = (array[min_index], array[ind])
+
 numList = []
-
-def main():
-    k = int(input("Enter number of elements to add :"))
-    for i in range (0,k):
-        n = int(input("Input element :"))
-        numList.append(n)
+k = int(input("Enter number of elements to add :"))
+for i in range (0,k):
+     n = int(input("Input element :"))
+     numList.append(n)
 
 
-
-
-main()
-selection_Sort(numList)
-for i in range(len(numList)):
-    print (numList[i], end=" ")
+size = len(numList)
+selectionSort(numList, size)
+print(numList)
